c2007cf7c9db8e796877a6a906fb89c2a56a525e
S1ler <igor.hrytsyk@gmail.com>
2025-11-05 14:52:31 +0200

    Add files via upload
    Changes

--- script.js
+++ script.js
@@ -15,26 +15,24 @@
     var socialsImage = document.querySelector('.socials__concert');
     var socialsBg = document.querySelector('.socials_bg');
     var socialLinks = document.querySelectorAll('.socials__link');
+    var socialsBuy = document.querySelector('.socials__buy--inner');
     var ANIMATION_CLASS = 'socials__link--animation';
 
     socialLinks.forEach(function (oneLink) {
       oneLink.classList.add(ANIMATION_CLASS);
     });
-    socialsBlock.style.display = 'none';
+/*     socialsBlock.style.display = 'none';
     socialsImage.style.display = 'block';
-    socialsBg.classList.add('socials_bg--blur');
+    socialsBg.classList.add('socials_bg--blur'); */
 
-  /* setTimeout(function () {
-    var socialsBlock = document.querySelector('.socials__title');
-    var socialsImage = document.querySelector('.socials__concert');
-    var socialsBg = document.querySelector('.socials_bg');
-    var socialsBuy = document.querySelector('.socials__buy--inner');
+   setTimeout(function () {
+    
     if (socialsBlock && socialsImage) {
-      //socialsBlock.style.display = 'none';
-      //socialsImage.style.display = 'block';
+      socialsBlock.style.display = 'none';
+      socialsImage.style.display = 'block';
       socialsBg.classList.add('socials_bg--blur');
     }
-  }, 2000); */
+  }, 2000);
   }
 
   function intersectionCallback(target, entries, observer) {
